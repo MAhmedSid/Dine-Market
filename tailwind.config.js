2b8584069a25f5ad3aa1a66c1480ffaef9a88c0d
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-07-12 13:12:53 +0500

    update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        pri_black: "#212121",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
